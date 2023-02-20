13a8d75745597f095d03d823a388d70db3cfdfe3
Michinded <michinded@gmail.com>
2023-02-20 16:58:20 -0600

    Creación de los métodos de la clase

--- Personaje.py
+++ Personaje.py
@@ -3,3 +3,18 @@ class Personaje:
     especie = "Humano"
     nombre = "Master Chief"
     altura = "2.70"
+
+    #Metodos del personaje
+    def correr(self, status):
+        if (status):
+            print("El personaje "+self.nombre+" esta corriendo")
+        else:
+            print("El personaje "+ self.nombre +"se detuvo")
+
+    def lanzarGranadas(self):
+        print("El personaje "+ self.nombre +" lanzo una granada")
+
+    def recargarArma(self, municiones):
+        cargador = 10
+        cargador += municiones
+        print("El personaje "+ self.nombre +" recargo su arma y ahora tiene "+ str(cargador) +" municiones")
