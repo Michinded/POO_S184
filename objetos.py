d818088dd026eebca5fdd7a9fb69be500ff76031
Michinded <michinded@gmail.com>
2023-02-20 17:08:59 -0600

    Creación de los objetos y llamarlos

---
+++ objetos.py
@@ -0,0 +1,10 @@
+from Personaje import *
+
+#1. Crear un objeto de la clase Personaje
+heroe = Personaje()
+
+#2. Usar atributos del objeto
+
+print("El personaje se llama: "+ heroe.nombre)
+print("El personaje es de la especie: "+ heroe.especie)
+print("El personaje mide: "+ heroe.altura+" metros")
