847d24b35f9323dd86b9caf43f6c69775a33450c
Michinded <michinded@gmail.com>
2023-03-30 02:43:26 -0600

    Validar email y retorno de resultados de busqueda
    Se agrego la validación de email para que no este repetido.
    
    Se cambio la función buscar para que ahora retorne el valor de la búsqueda.

--- SQLite/Practica15/app/Logica.py
+++ SQLite/Practica15/app/Logica.py
@@ -10,6 +10,16 @@ class Logic():
         conexion = Conexion()
         #encriptamos la contraseña
         password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        #Verificamos que el correo no este registrado
+        consulta_tipo_1 = "SELECT * FROM TBRegistros WHERE email = ?"
+        valores = (email,)
+        cursor = conexion.cursor
+        cursor.execute(consulta_tipo_1, valores)
+        resultados = cursor.fetchall()
+        if len(resultados) > 0:
+            messagebox.showerror("Error", "El correo ya esta registrado")
+            return
+        #insertamos los datos en la base de datos
         #el id se autoincrementa
         consulta = "INSERT INTO TBRegistros (nombre, email, password) VALUES (?, ?, ?)"
         valores = (nombre, email, password)
@@ -38,13 +48,8 @@ class Logic():
             messagebox.showerror("Error", "No se encontró ningún usuario con los datos especificados")
             return
 
-        mensaje = ""
-        for fila in resultados:
-            mensaje += f"ID: {fila[0]}\n"
-            mensaje += f"Nombre: {fila[1]}\n"
-            mensaje += f"Email: {fila[2]}\n"
-
-        messagebox.showinfo("Resultados", mensaje)
+        #enviamos los datos a la ventana retornando una lista con los datos
+        return resultados
 
     def actualizar(self, id, nombre, email, password):
         conexion = Conexion()
